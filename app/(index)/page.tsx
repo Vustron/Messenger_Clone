6883ab3ab1ef7a9c138b2bc592ff93f0dfaa79e7
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-02-08 10:52:53 +0800

    feat: Added custom logo and initial Auth UI

---
+++ app/(index)/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+export default function Index() {
+	return (
+		<div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
+			<div className='sm:mx-auto sm:w-full sm:max-w-md'>
+				<Image
+					className='mx-auto w-auto'
+					alt='Logo'
+					height='48'
+					width='48'
+					src='/images/logo.png'
+				/>
+
+				<h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+					Sign in to your account
+				</h2>
+				{/* AuthForm */}
+			</div>
+		</div>
+	);
+}
