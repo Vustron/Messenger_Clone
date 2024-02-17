762e52f76e873bc2e2d9df9657f8d102c93966f7
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-02-17 17:08:16 +0800

    feat: Added loading state

--- components/sidebar/UserBox.tsx
+++ components/sidebar/UserBox.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import LoadingModal from '@/components/modals/LoadingModal';
 import { UserBoxProps } from '@/lib/interfaces';
 import { useCallback, useState } from 'react';
 import Avatar from '@/components/ui/Avatar';
@@ -27,21 +28,25 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
 	}, [data, router]);
 
 	return (
-		<div
-			className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
-			onClick={handleClick}
-		>
-			<Avatar user={data} />
-			<div className='min-w-0 flex-1'>
-				<div className='focus:outline-none'>
-					<div className='flex justify-between items-center mb-1'>
-						<span className='text-sm font-medium text-gray-900'>
-							{data.name}
-						</span>
+		<>
+			{isLoading && <LoadingModal />}
+
+			<div
+				className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
+				onClick={handleClick}
+			>
+				<Avatar user={data} />
+				<div className='min-w-0 flex-1'>
+					<div className='focus:outline-none'>
+						<div className='flex justify-between items-center mb-1'>
+							<span className='text-sm font-medium text-gray-900'>
+								{data.name}
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
